33b3bf4567a89479fa71bb6b5368e2dc0aa58232
Caio Marquezini <105600461+caion90@users.noreply.github.com>
2024-09-24 15:18:37 -0300

    Update cors.java

--- java-backend/src/main/java/com/empregados/app/cors.java
+++ java-backend/src/main/java/com/empregados/app/cors.java
@@ -1,11 +1,1 @@
-@Configuration
-public class CorsConfiguration implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("http://localhost:8000")
-            .allowedOrigins("http://localhost:8080")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-    }
-}
