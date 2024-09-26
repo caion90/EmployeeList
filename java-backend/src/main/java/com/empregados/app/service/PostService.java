b81fe9facdac086e64b609191c82c02ee06b8cdc
caion90 <caionmarquezini90@gmail.com>
2024-09-26 14:45:56 -0300

    adding interfaces to post message

--- java-backend/src/main/java/com/empregados/app/service/PostService.java
+++ java-backend/src/main/java/com/empregados/app/service/PostService.java
@@ -1,5 +1,23 @@
 package com.empregados.app.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import com.empregados.app.entity.PostEntity;
+import com.empregados.app.repository.PostRepository;
+
 public class PostService {
+
+    @Autowired
+    PostRepository postRepository;
+
+    public Page<PostEntity> findAll() {
+        PageRequest Pageable = PageRequest.of(0, 10);
+        return postRepository.findAll(Pageable);
+    }
 
 }
