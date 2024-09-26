397de32d6f08c9111b7ca123614f927eed96810f
Caio Marquezini <105600461+caion90@users.noreply.github.com>
2024-09-26 15:18:12 -0300

    Update Demo1Application.java

--- java-backend/src/main/java/com/empregados/app/Demo1Application.java
+++ java-backend/src/main/java/com/empregados/app/Demo1Application.java
@@ -6,10 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.empregados.app.entity.PostEntity;
-import com.empregados.app.repository.PostRepository;
-import com.empregados.app.service.PostService;
-
 @SpringBootApplication
 public class Demo1Application {
 
